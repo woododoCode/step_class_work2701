8ec1a97d515d4584a64b0a53770e64d5fa2b9396
Ihor Mynenko <woododo2@gmail.com>
2022-01-27 20:41:59 +0200

    Added new functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Human:
-    def __init__(self, name, age):
-        self.__name = name
-        self.__age = age
+    def __init__(self, **kwargs):
+        self.__name = kwargs.get('name')
+        self.__age = kwargs.get('age')
+        self.__height = kwargs.get('height')
 
     def get_name(self):
         return self.__name
@@ -9,11 +10,17 @@ class Human:
     def set_name(self, name):
         self.__name = name
 
+    def update_info(self, **kwargs):
+        self.__name = kwargs.get('name') if kwargs.get('name') else self.__name
+        self.__age = kwargs.get('age') if kwargs.get('age') else self.__age
+        self.__height = kwargs.get('height') if kwargs.get('height') else self.__height
+
     def __repr__(self):
-        return f"Name: {self.__name}, age: {self.__age}"
+        return f"Name: {self.__name}, age: {self.__age}, height: {self.__height}"
 
 
-human = Human('Mike', 15)
-print(human.get_name())
-human.set_name('Lis')
-print(human.get_name())
+human = Human(age=15, name='Mike', height=135)
+d = {"age": 15, "name": 'Mike'}
+print(human)
+human.update_info(name="Lisa")
+print(human)
